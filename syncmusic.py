4716553442ba88768fce218d71c44f9d80001904
Andrew Hurle <fracture91@gmail.com>
2013-01-09 23:37:43 -0500

    syncmusic: fix bug where script didn't wait until partition was mounted

--- syncmusic.py
+++ syncmusic.py
@@ -10,12 +10,20 @@ import argparse
 Need to make sure the windows partition is mounted, since that's where fb2k lives.
 Quite a hack here.  I want winpart's wrapping behavior, but what can I wrap?  the tail command will
 wait for content in a file that doesn't exist, and it will terminate when the process with the
-indicated pid terminates, so I can give it the current pid to wait until the script is finished!
+indicated pid terminates. So, I can give it the current pid to wait until the script is finished!
 """
 DEVNULL = open(os.devnull, "w")
-subprocess.Popen(["winpart", "-w", "tail", "-F", "--pid=" + str(os.getpid()), str(uuid.uuid4())],
-                 stdin=None, stdout=DEVNULL, stderr=DEVNULL)
+command = ["winpart", "-w", "tail", "-F", "--pid=" + str(os.getpid()), str(uuid.uuid4())]
+proc = subprocess.Popen(command, stdin=None, stdout=DEVNULL, stderr=subprocess.PIPE)
 
+"""
+But how do I wait for the partition to be mounted?  It won't be ready until tail starts running.
+Since tail's file doesn't exist, it will print an error message to stderr, which I can eat here!
+"""
+for line in proc.stderr:
+	break
+
+# and now I know that the partition is mounted
 
 config = fplsync.Config()
 
