f88482e097d2f5f514d4753bbec061744176337a
Andrew Hurle <fracture91@gmail.com>
2014-12-21 03:42:41 -0500

    Add git/git-gui apt-get install to git install script

--- install/git.sh
+++ install/git.sh
@@ -4,3 +4,11 @@ ln -s `pwd`/gitconfig ~/.gitconfig
 ln -s `pwd`/git-completion.sh ~/.git-completion.sh
 ln -s `pwd`/git-prompt.sh ~/.git-prompt.sh
 
+if [ `which apt-get` ]; then
+  sudo apt-get install git git-gui
+fi
+
+# see https://github.com/github/hub/releases for installing hub
+# I was on 2.2.0-preview1 as of this writing
+# installation process is slightly too sketchy to automate
+
