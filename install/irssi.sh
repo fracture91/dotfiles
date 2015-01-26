2feb5977c781bcd5677437324833040169124919
Andrew Hurle <fracture91@gmail.com>
2015-01-25 23:36:41 -0500

    irssi: Add a level of indirection to desktop file to allow remote connection on Tarsonis

--- install/irssi.sh
+++ install/irssi.sh
@@ -8,3 +8,6 @@ ln -s `pwd`/irssi/irssi.png ~/.local/share/icons/irssi.png
 ln -s `pwd`/irssi/irssi.desktop ~/.local/share/applications/irssi.desktop
 ln -s `pwd`/irssi ~/.irssi
 
+# you might choose to replace this symlink with a script like `~/.irssi/remote Aiur.local`
+ln -s `pwd`/irssi/screen ./irssi/run-desktop
+
