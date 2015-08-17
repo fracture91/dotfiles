ae37f219b6bce341b68da11c3e16e46403896269
Andrew Hurle <fracture91@gmail.com>
2015-08-16 21:39:02 -0400

    Add vlc to desktop utils

--- install/desktop-utils.sh
+++ install/desktop-utils.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
 if [ `which apt-get` ]; then
-  sudo apt-get install chromium-browser puddletag deluge fbreader
+  sudo apt-get install chromium-browser puddletag deluge fbreader vlc
 fi
