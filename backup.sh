bbff1b8ca679b663a0892bffbe393b8b274a7664
Andrew Hurle <fracture91@gmail.com>
2021-01-04 19:03:53 -0500

    Point backup script to Xil instead of Tassadar

--- backup.sh
+++ backup.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
-(rsync -rltnDu --modify-window=1 --progress --delete ~/.dotfiles/ /media/Tassadar/.dotfiles 2>&1) | tee backup-output.txt
+(rsync -rltnDu --modify-window=1 --progress --delete ~/.dotfiles/ /media/ahurle/Xil/backups/.dotfiles 2>&1) | tee backup-output.txt
 
